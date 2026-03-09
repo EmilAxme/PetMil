1f5674a0e1d0574323bc3b87900bd84feb4bcf01
EmilAxme <emil.azmedov@mail.ru>
2026-03-09 15:37:27 +0300

    Создал WeatherAssembly

---
+++ PetMil/PetMil/WeatherAssembly.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherAssembly.swift
+//  PetMil
+//
+//  Created by Emil on 08.03.2026.
+//
+
+import UIKit
+
+enum WeatherAssembly {
+    static func build() -> UIViewController {
+        let viewController = WeatherViewController()
+        let presenter = WeatherPresenter()
+        
+        viewController.presenter = presenter
+        presenter.view = viewController
+    }
+}
