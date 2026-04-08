cfd4633aa4d1071cf8084164cedc251d008becd2
EmilAxme <emil.azmedov@mail.ru>
2026-04-08 16:30:33 +0300

    сверстал экран выбора города

--- PetMil/PetMil/Modules/CitySearch/CitySearchViewController.swift
+++ PetMil/PetMil/Modules/CitySearch/CitySearchViewController.swift
@@ -8,27 +8,102 @@
 import UIKit
 
 protocol CitySearchViewProtocol: AnyObject {
-    func displayTitle(_ title: String)
+    func displayCities(_ viewModel: CitySearchModels.ViewModel)
+    func displaySelectedCity(_ cityName: String)
 }
 
 final class CitySearchViewController: UIViewController {
     
     var presenter: CitySearchPresenterProtocol?
     
+    private var cities: [CitySearchModels.City] = []
+    
+    private lazy var searchController: UISearchController = {
+        let controller = UISearchController(searchResultsController: nil)
+        controller.obscuresBackgroundDuringPresentation = false
+        controller.searchBar.placeholder = "Search city"
+        controller.searchResultsUpdater = self
+        return controller
+    }()
+    
+    private lazy var cityTableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .plain)
+        tableView.backgroundColor = .clear
+        tableView.separatorStyle = .none
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 76
+        tableView.showsVerticalScrollIndicator = false
+        tableView.register(CityResultCell.self, forCellReuseIdentifier: CityResultCell.reuseIdentifier)
+        return tableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupAppearance()
+        setupLayout()
         presenter?.viewDidLoad()
     }
-    
-    private func setupAppearance() {
+}
+
+private extension CitySearchViewController {
+    func setupAppearance() {
         view.backgroundColor = .systemBackground
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = false
+        definesPresentationContext = true
+    }
+    
+    func setupLayout() {
+        view.addToView(cityTableView)
+        
+        NSLayoutConstraint.activate([
+            cityTableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            cityTableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            cityTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            cityTableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
     }
 }
 
 extension CitySearchViewController: CitySearchViewProtocol {
+    func displayCities(_ viewModel: CitySearchModels.ViewModel) {
+        cities = viewModel.cities
+        cityTableView.reloadData()
+    }
     
-    func displayTitle(_ title: String) {
-        self.title = title
+    func displaySelectedCity(_ cityName: String) {
+        tabBarController?.selectedIndex = 1
+    }
+}
+
+extension CitySearchViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cities.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        guard let cell = tableView.dequeueReusableCell(
+            withIdentifier: CityResultCell.reuseIdentifier,
+            for: indexPath
+        ) as? CityResultCell else {
+            return UITableViewCell()
+        }
+        
+        cell.configure(with: cities[indexPath.row])
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        presenter?.didSelectCity(at: indexPath.row)
+    }
+}
+
+extension CitySearchViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        presenter?.didUpdateSearch(text: searchController.searchBar.text ?? "")
     }
 }
