2ed608cf547ee1a8593ae2f83837f4fb6f453720
EmilAxme <emil.azmedov@mail.ru>
2026-04-08 12:50:13 +0300

    создал DayDetailMOdel

---
+++ PetMil/PetMil/Modules/DayDetail/DayDetailModels.swift
@@ -0,0 +1,20 @@
+//
+//  DayDetailModels.swift
+//  PetMil
+//
+//  Created by Emil on 07.04.2026.
+//
+
+import Foundation
+
+enum DayDetailsModels {
+    struct ViewModel {
+        let dayText: String
+        let temperatureText: String
+        let descriptionText: String
+        let humidityText: String
+        let windText: String
+        let feelsLikeText: String
+        let pressureText: String
+    }
+}
