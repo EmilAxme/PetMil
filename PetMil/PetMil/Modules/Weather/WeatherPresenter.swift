bda3ea9629a2371487ac96abab7800721ea110e6
EmilAxme <emil.azmedov@mail.ru>
2026-04-08 15:38:56 +0300

    изменил структуру presenter и обработал отображение города в погоде

--- PetMil/PetMil/Modules/Weather/WeatherPresenter.swift
+++ PetMil/PetMil/Modules/Weather/WeatherPresenter.swift
@@ -9,53 +9,69 @@ import Foundation
 
 protocol WeatherPresenterProtocol: AnyObject {
     func viewDidLoad()
+    func viewWillAppear()
 }
 
-final class WeatherPresenter: WeatherPresenterProtocol {
+final class WeatherPresenter {
     
     weak var view: WeatherViewProtocol?
     
     private let storage: SelectedCityStorageProtocol
+    
     init(storage: SelectedCityStorageProtocol) {
         self.storage = storage
     }
 }
+    
+    
+extension WeatherPresenter: WeatherPresenterProtocol {
     func viewDidLoad() {
+        updateWeather()
+    }
+    
+    func viewWillAppear() {
+        updateWeather()
+    }
+}
+
+private extension WeatherPresenter {
+    func updateWeather() {
+        let selectedCity = storage.selectedCity
         
         let viewModel = WeatherModels.ViewModel(
-                    city: "Moscow",
-                    currentTemperature: "12°",
-                    currentDescription: "Cloudy",
-                    rows: [
-                        .init(
-                            dayText: "Today",
-                            temperatureText: "12°",
-                            descriptionText: "Cloudy",
-                            humidityText: "78%",
-                            windText: "5 m/s",
-                            feelsLikeText: "10°",
-                            pressureText: "1012 hPa"
-                        ),
-                        .init(
-                            dayText: "Tomorrow",
-                            temperatureText: "10°",
-                            descriptionText: "Rain",
-                            humidityText: "85%",
-                            windText: "7 m/s",
-                            feelsLikeText: "8°",
-                            pressureText: "1008 hPa"
-                        ),
-                        .init(
-                            dayText: "Friday",
-                            temperatureText: "14°",
-                            descriptionText: "Sunny",
-                            humidityText: "60%",
-                            windText: "3 m/s",
-                            feelsLikeText: "13°",
-                            pressureText: "1015 hPa"
-                        )
-                    ]
+            city: selectedCity,
+            currentTemperature: "12°",
+            currentDescription: "Cloudy",
+            rows: [
+                .init(
+                    dayText: "Today",
+                    temperatureText: "12°",
+                    descriptionText: "Cloudy",
+                    humidityText: "78%",
+                    windText: "5 m/s",
+                    feelsLikeText: "10°",
+                    pressureText: "1012 hPa"
+                ),
+                .init(
+                    dayText: "Tomorrow",
+                    temperatureText: "10°",
+                    descriptionText: "Rain",
+                    humidityText: "85%",
+                    windText: "7 m/s",
+                    feelsLikeText: "8°",
+                    pressureText: "1008 hPa"
+                ),
+                .init(
+                    dayText: "Friday",
+                    temperatureText: "14°",
+                    descriptionText: "Sunny",
+                    humidityText: "60%",
+                    windText: "3 m/s",
+                    feelsLikeText: "13°",
+                    pressureText: "1015 hPa"
                 )
+            ]
+        )
         
         view?.displayWeather(viewModel: viewModel)
     }
