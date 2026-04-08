9a687fa9c17ee5822f1388fe45dbf11c489050f8
EmilAxme <emil.azmedov@mail.ru>
2026-04-08 16:42:45 +0300

    Написал NetworkClient

---
+++ PetMil/PetMil/Network/NetworkClient.swift
@@ -0,0 +1,69 @@
+//
+//  NetworkClient.swift
+//  PetMil
+//
+//  Created by Emil on 08.04.2026.
+//
+
+import Foundation
+
+protocol NetworkClientProtocol: AnyObject {
+    func request<T: Decodable>(_ endpoint: Endpoint, type: T.Type) async throws -> T
+}
+
+final class NetworkClient: NetworkClientProtocol {
+    
+    private let session: URLSession
+    
+    init(session: URLSession = .shared) {
+        self.session = session
+    }
+}
+
+extension NetworkClient {
+    func request<T: Decodable>(_ endpoint: Endpoint, type: T.Type) async throws -> T {
+        guard let urlRequest = try? makeURLRequest(from: endpoint) else {
+            throw APIError.invalidURL
+        }
+        
+        let (data, response): (Data, URLResponse)
+        
+        do {
+            (data, response) = try await session.data(for: urlRequest)
+        } catch {
+            throw APIError.requestFailed
+        }
+        
+        guard let httpResponse = response as? HTTPURLResponse else {
+            throw APIError.invalidResponse
+        }
+        
+        guard 200...299 ~= httpResponse.statusCode else {
+            throw APIError.serverError(statusCode: httpResponse.statusCode)
+        }
+        
+        do {
+            return try JSONDecoder().decode(T.self, from: data)
+        } catch {
+            throw APIError.decodingFailed
+        }
+    }
+}
+
+private extension NetworkClient {
+    func makeURLRequest(from endpoint: Endpoint) throws -> URLRequest {
+        var components = URLComponents()
+        components.scheme = endpoint.scheme
+        components.host = endpoint.host
+        components.path = endpoint.path
+        components.queryItems = endpoint.queryItems
+        
+        guard let url = components.url else {
+            throw APIError.invalidURL
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = endpoint.method.rawValue
+        return request
+    }
+}
