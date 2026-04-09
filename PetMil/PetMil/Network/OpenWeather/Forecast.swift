6a586bdb4490e8bb2b88e7267a6d6cdbdb43cae1
EmilAxme <emil.azmedov@mail.ru>
2026-04-09 12:41:12 +0300

    Добавил модель отдельного дня

---
+++ PetMil/PetMil/Network/OpenWeather/Forecast.swift
@@ -0,0 +1,25 @@
+//
+//  Forecast.swift
+//  PetMil
+//
+//  Created by Emil on 08.04.2026.
+//
+
+import Foundation
+
+struct Forecast {
+    let cityName: String
+    let items: [ForecastItem]
+}
+
+struct ForecastItem {
+    let date: Date
+    let temperature: Double
+    let feelsLike: Double
+    let pressure: Int
+    let humidity: Int
+    let windSpeed: Double
+    let title: String
+    let description: String
+    let iconCode: String
+}
