75427e3b85de1bb59e3952ba76503090e48b3b18
EmilAxme <emil.azmedov@mail.ru>
2026-04-09 12:40:58 +0300

    Добавил обработку отдельного дня из API OpenWeather

--- PetMil/PetMil/Services/WeatherService.swift
+++ PetMil/PetMil/Services/WeatherService.swift
@@ -9,6 +9,7 @@ import Foundation
 
 protocol WeatherServiceProtocol: AnyObject {
     func fetchCityLocation(for city: String) async throws -> CityLocation
+    func fetchForecast(lat: Double, lon: Double) async throws -> Forecast
 }
 
 final class WeatherService: WeatherServiceProtocol {
@@ -41,4 +42,28 @@ extension WeatherService {
             country: firstCity.country
         )
     }
+    
+    func fetchForecast(lat: Double, lon: Double) async throws -> Forecast {
+            let endpoint = OpenWeatherEndpoint.forecast(lat: lat, lon: lon, apiKey: apiKey)
+            let response = try await networkClient.request(endpoint, type: ForecastResponseDTO.self)
+            
+            let items = response.list.map {
+                ForecastItem(
+                    date: Date(timeIntervalSince1970: $0.dt),
+                    temperature: $0.main.temp,
+                    feelsLike: $0.main.feelsLike,
+                    pressure: $0.main.pressure,
+                    humidity: $0.main.humidity,
+                    windSpeed: $0.wind.speed,
+                    title: $0.weather.first?.main ?? "",
+                    description: $0.weather.first?.description ?? "",
+                    iconCode: $0.weather.first?.icon ?? ""
+                )
+            }
+            
+            return Forecast(
+                cityName: response.city.name,
+                items: items
+            )
+        }
 }
