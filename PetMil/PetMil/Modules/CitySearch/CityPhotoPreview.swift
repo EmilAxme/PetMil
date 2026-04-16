a4ab82b58f9978874a705ecdec248cf6a2cf0c5d
EmilAxme <emil.azmedov@mail.ru>
2026-04-16 16:10:15 +0300

    Добавил модель CityPhotoPreview

---
+++ PetMil/PetMil/Modules/CitySearch/CityPhotoPreview.swift
@@ -0,0 +1,15 @@
+//
+//  CityPhotoPreview.swift
+//  PetMil
+//
+//  Created by Emil on 15.04.2026.
+//
+
+import Foundation
+
+struct CityPhotoPreview {
+    let imageURL: URL
+    let authorName: String
+    let authorProfileURL: URL
+    let photoPageURL: URL
+}
