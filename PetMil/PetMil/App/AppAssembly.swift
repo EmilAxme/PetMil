605bd745ebea8ee58b01b337e46a3052303ab8b5
EmilAxme <emil.azmedov@mail.ru>
2026-05-29 01:45:58 +0300

    F4: AppAssembly использует WeatherPageContainerViewController
    - AppAssembly возвращает page container на вкладке Weather
    - Удалён старый SelectedCityStorage class и SelectedCityStorageProtocol
      (заменён на CityListStorage из предыдущих коммитов F4)
    - SelectedCity struct переехал в отдельный файл Core/SelectedCity.swift

--- PetMil/PetMil/App/AppAssembly.swift
+++ PetMil/PetMil/App/AppAssembly.swift
@@ -10,27 +10,29 @@ import UIKit
 enum AppAssembly {
     static func build() -> UIViewController {
         let tabBarController = AppTabBarController()
-        
+
         let citySearchViewController = CitySearchAssembly.build()
-        let weatherViewController = WeatherAssembly.build()
-        
-        let weatherNavigationController = UINavigationController(rootViewController: weatherViewController)
+        let weatherPageContainer = WeatherPageContainerViewController(
+            cityListStorage: CityListStorage.shared
+        )
+
+        let weatherNavigationController = UINavigationController(rootViewController: weatherPageContainer)
         let citySearchNavigationController = UINavigationController(rootViewController: citySearchViewController)
-        
+
         weatherNavigationController.tabBarItem = UITabBarItem(
             title: "Weather",
             image: UIImage(systemName: "cloud.sun.fill"),
             selectedImage: UIImage(systemName: "cloud.sun.fill")
         )
-        
+
         citySearchNavigationController.tabBarItem = UITabBarItem(
             title: "Search City",
             image: UIImage(systemName: "magnifyingglass"),
             selectedImage: UIImage(systemName: "magnifyingglass")
         )
-        
+
         tabBarController.setViewControllers([citySearchNavigationController, weatherNavigationController], animated: false)
-        
+
         return tabBarController
     }
 }
--- PetMil/PetMil/Core/SelectedCity.swift
+++ PetMil/PetMil/Core/SelectedCity.swift
@@ -1,5 +1,5 @@
 //
-//  SelectedCityStorage.swift
+//  SelectedCity.swift
 //  PetMil
 //
 //  Created by Emil on 08.04.2026.
@@ -43,52 +43,5 @@ struct SelectedCity: Codable {
         longitude = try container.decode(Double.self, forKey: .longitude)
         photoURLString = try container.decodeIfPresent(String.self, forKey: .photoURLString)
         isCurrentLocation = try container.decodeIfPresent(Bool.self, forKey: .isCurrentLocation) ?? false
-    }
-}
-
-protocol SelectedCityStorageProtocol: AnyObject {
-    var selectedCity: SelectedCity { get set }
-    var hasSelectedCity: Bool { get }
-}
-
-final class SelectedCityStorage: SelectedCityStorageProtocol {
-    
-    static let shared = SelectedCityStorage()
-    
-    private let userDefaults: UserDefaults
-    private let selectedCityKey = "selected_city_key"
-    
-    private let defaultCity = SelectedCity(
-        name: "Moscow",
-        country: "RU",
-        latitude: 55.7504461,
-        longitude: 37.6174943,
-        photoURLString: nil,
-        isCurrentLocation: false
-    )
-    
-    var selectedCity: SelectedCity {
-        get {
-            guard
-                let data = userDefaults.data(forKey: selectedCityKey),
-                let city = try? JSONDecoder().decode(SelectedCity.self, from: data)
-            else {
-                return defaultCity
-            }
-            
-            return city
-        }
-        set {
-            guard let data = try? JSONEncoder().encode(newValue) else { return }
-            userDefaults.set(data, forKey: selectedCityKey)
-        }
-    }
-    
-    var hasSelectedCity: Bool {
-        userDefaults.data(forKey: selectedCityKey) != nil
-    }
-
-    private init(userDefaults: UserDefaults = .standard) {
-        self.userDefaults = userDefaults
     }
 }
